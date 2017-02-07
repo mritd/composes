57aad563307e5dba7f3a8c9a6873adbb4387597a
mritd <mritd@mritd.me>
2017-02-07 13:01:21 +0800

    修正 mmuser 默认密码错误

--- mattermost/init/init.sql
+++ mattermost/init/init.sql
@@ -1,3 +1,3 @@
-create user 'mmuser'@'%' identified by 'mmuser';
+create user 'mmuser'@'%' identified by 'mostest';
 create database mattermost;
 grant all privileges on mattermost.* to 'mmuser'@'%';
