e74d6a9740be6e73fbff85930e6fc8f916bcc435
mritd <mritd1234@gmail.com>
2017-06-01 23:39:39 +0800

    超时便重启

--- tcpspeed/tcp_status.sh
+++ tcpspeed/tcp_status.sh
@@ -3,25 +3,16 @@
 export http_proxy=http://192.168.1.20:8123
 export https_proxy=http://192.168.1.20:8123
 
-failedNum=0
-
 while true;do
-
-    if [ $failedNum -gt 2 ];then
-        echo -e "\033[33mrestart tcpspeed...\033[0m"
-        ssh root@ss.x123.me /xs/restart.sh
-        (cd /home/docker-compose/tcpspeed && dc down && dc up -d)
-        failedNum=0
-    fi
 
     sleep 10
     curl --connect-timeout 10 -m 10 -iv https://www.google.com > /dev/null 2>&1
     if [ "$?" != "0" ];then
         echo -e "`date "+%Y-%m-%d %H:%M:%S"`:""\033[31m tcpspeed connection timeout...\033[0m"
-        let failedNum++
+        ssh root@ss.x123.me /xs/restart.sh
+        (cd /home/docker-compose/tcpspeed && dc down && dc up -d)
     else
         echo -e "`date "+%Y-%m-%d %H:%M:%S"`:""\033[32m tcpspeed is ok...\033[0m"
-        failedNum=0
     fi
 
 done
