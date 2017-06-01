c12a278e08775cf1698695abd75c3f0cf54d0be4
mritd <mritd1234@gmail.com>
2017-06-01 23:59:59 +0800

    增加异地监测互斥

--- tcpspeed/tcp_status.sh
+++ tcpspeed/tcp_status.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
+cur_location=$1
 export http_proxy=http://192.168.1.20:8123
 export https_proxy=http://192.168.1.20:8123
 
+if [ "$cur_location" == "" ];then
+    echo -e "\033[31mError: location is blank!\033[0m"
+    exit 1
+fi
+
+ssh root@ss.x123.me "echo $cur_location > /tmp/monitor_location"
+
 while true;do
+    
+    remote_location=`ssh root@ss.x123.me cat /tmp/monitor_location`
+
+    if [ "$remote_location" != "$cur_location" ];then
+        echo -e "\033[33mswitch monitor...\033[0m"
+        exit 0
+    fi
 
     sleep 10
     curl --connect-timeout 10 -m 10 -iv https://www.google.com > /dev/null 2>&1
@@ -14,5 +29,4 @@ while true;do
     else
         echo -e "`date "+%Y-%m-%d %H:%M:%S"`:""\033[32m tcpspeed is ok...\033[0m"
     fi
-
 done
